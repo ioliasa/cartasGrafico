82ae6b567eaab016d359ba8774ed2a924e7c2201
Inma <ioliasa@gmail.com>
2022-03-03 19:40:33 +0100

    Primer commit

---
+++ src/com/jacaranda/sieteYmedia/mainJuegoGUI.java
@@ -0,0 +1,15 @@
+package com.jacaranda.sieteYmedia;
+
+import java.util.Scanner;
+
+public class mainJuegoGUI {
+
+	public static Scanner teclado = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		// Lanza la clase JuegoGUI
+		
+		JuegoGUI juego = new JuegoGUI();
+			
+	}
+}
